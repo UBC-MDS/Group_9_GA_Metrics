3827acc88679d076892d7e5c058b9399f10b1356
Beth Ou Yang <beth.ouyang@gmail.com>
2024-01-11 19:17:35 -0800

    Add docstring for stat-summary functions

---
+++ src/dscigametrics/metric2.py
@@ -0,0 +1,20 @@
+
+def summary_metric2(metric2, verbose = True):
+    """Return statistics summary of ______
+
+    Parameters
+    ----------
+    metric2 : pd.Series
+        Serveral data points represent `metric2` of different time.
+    verbose : boolean
+        Whether or not print out the statistics summary result.
+
+    Returns
+    -------
+    mean
+        Mean value of the data points.
+    median
+        Median value of the data points.
+    sd
+        Standard deviation of the data points.
+    """
