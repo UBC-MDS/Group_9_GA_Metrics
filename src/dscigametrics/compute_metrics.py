b215e8d707f6029701556c765f2a6868914f7d91
ianm99 <umeabor@student.ubc.ca>
2024-01-12 14:22:55 -0800

    weilin changes

---
+++ src/dscigametrics/compute_metrics.py
@@ -0,0 +1,28 @@
+def compute_metrics(data, campaign_id, start_date, end_date):
+    '''
+    
+    Computes four metrics from Google Analytics data: 
+        1. New to return rate
+        2. Conversion rate
+        3. Total transaction revenue
+        4. Average transaction revenue
+
+    Parameters
+    ----------
+
+    data : dataframe
+        Dataframe containing information from google analytics
+    campaign_id : int
+        The campaign ID of the campaign in question
+    start_date : int
+        Date when campaign started
+    end_date : int
+        Date when campaign ended
+
+    Returns
+    -------
+
+    string
+        A body of text stating the four computed metrics
+        
+    '''
