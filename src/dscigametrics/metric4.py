967c611e8e6c83a3a49c2153714d89a5cc6f5b62
tang yili <yltang92@student.ubc.ca>
2024-01-12 14:48:09 -0800

    small changes in first two line

--- src/dscigametrics/metric4.py
+++ src/dscigametrics/metric4.py
@@ -1,7 +1,6 @@
 def find_best_and_worst_campaigns(df, start_date, end_date, campaign_ids, metric):
     """
     Analyzes marketing campaign performance over a specified date range and identifies the best and worst performing campaigns based on a selected metric.
-
     This function iterates over a list of campaign IDs, calculates performance metrics for each campaign within the given date range, and then determines the best and worst performing campaigns based on the specified metric.
 
     Parameters:
