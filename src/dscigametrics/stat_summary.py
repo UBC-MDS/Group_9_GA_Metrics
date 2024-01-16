4971c5afae51a6cf89c311792fdc593814d3ff91
Beth Ou Yang <beth.ouyang@gmail.com>
2024-01-15 22:08:27 -0800

    update stat_summay function

--- src/dscigametrics/stat_summary.py
+++ src/dscigametrics/stat_summary.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+
 def stat_summary(data, campaign_id, start_date, end_date):
     """Return statistics summary of metrics.
     
@@ -38,4 +41,25 @@ def stat_summary(data, campaign_id, start_date, end_date):
         SD           1.43        0.66         1.62          0.77
     """
 
+    def get_return_rate(data):
+        return data['totals.newVisits'].fillna(0.0).mean()
+    def get_conversion(data):
+        return sum(data['totals.transactions'].fillna(0.0)) / sum(data['totals.visits'])
     
+    data = data[(data['trafficSource.adwordsClickInfo.campaignId'] == campaign_id) & (data['date'] >= start_date) & (data['date'] <= end_date)]
+
+    return_rates = data.groupby(['date'])[['date', 'totals.newVisits']].apply(get_return_rate).reset_index()[0].to_list()
+    conversion_rates = data.groupby(['date'])[['date', 'totals.transactions']].apply(get_conversion).reset_index()[0].to_list()
+    ttl_transac_revenues = data.fillna(0.0).groupby(['date'])['totals.transactionRevenue'].sum().reset_index().iloc[:, 1].to_list()
+    avg_transac_revenues = data.fillna(0.0).groupby(['date'])['totals.transactionRevenue'].mean().reset_index().iloc[:, 1].to_list()
+
+    output = pd.DataFrame(
+                {
+                    'return_rate': [return_rates.mean(), return_rates.median(),return_rates.std()],
+                    'conversion_rate': [conversion_rates.mean(), conversion_rates.median(),conversion_rates.std()],
+                    'ttl_revenue': [ttl_transac_revenues.mean(), ttl_transac_revenues.median(),ttl_transac_revenues.std()],
+                    'avg_revenue': [avg_transac_revenues.mean(), avg_transac_revenues.median(),avg_transac_revenues.std()]
+                },
+                index = ['Mean', 'Median', 'Standard Deviation']
+            )
+    return output
