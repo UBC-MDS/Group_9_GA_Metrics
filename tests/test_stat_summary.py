86298c1702bae8cb0bb540938b4780e6c17a6de2
Beth Ou Yang <beth.ouyang@gmail.com>
2024-01-18 18:17:44 -0800

    add comments

--- tests/test_stat_summary.py
+++ tests/test_stat_summary.py
@@ -11,16 +11,20 @@ data = pd.read_csv(test_data_path)
 
 output = stat_summary(data, campaign_id, start_date, end_date)
 
+# Test for correct return value of mean
 def test_mean():
     assert output.iloc[0, 0] == (0.9 + 0.8) /2, f"The mean value is wrong with test data: {test_data_path}"
 
+# Test for correct return value of standard deviation
 def test_std():
     assert np.isclose(output.iloc[2, 0], np.std([0.9, 0.8]), rtol=1e-05, atol=1e-08, equal_nan=False).all(), f"The standard deviation value is wrong with test data: {test_data_path}"
 
+# Test for the raised error when there is unexpected input type
 def test_input_type_error():
     with pytest.raises(TypeError):
         stat_summary('data', campaign_id, start_date, end_date)
 
+# Test for correct handling for situation where all values are zeros in 'totals.visits', to avoid devided by zero.
 def test_all_zero():
     data['totals.visits'] = 0
     output = stat_summary(data, campaign_id, start_date, end_date)
